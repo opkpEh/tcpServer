0b054e772fd0d69100d67f4b4fc7f2737e0a4d3f
opkpEh <harshpandeykp20@gmail.com>
2024-11-02 02:48:53 +0530

    threading added

--- server/main.py
+++ server/main.py
@@ -2,41 +2,46 @@
 import socket
 from pynput import keyboard
 import sys
+import _thread
 
 def on_press(key):
     if key== keyboard.KeyCode.from_char('q'):
-        s.close()
+        server_socket.close()
         sys.exit()
 
 #start listening for keyboard inputs
 listener = keyboard.Listener(on_press=on_press)
 listener.start()
 
-s= socket.socket()
+server_socket= socket.socket()
 
 #to prevent the socket being left in time wait state
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 #the server is listening on port 8080 on any network
-s.bind(("0.0.0.0", 8080))
+server_socket.bind(("0.0.0.0", 8080))
 
-s.listen(10)#server can have a backlog connection requests of 10
+server_socket.listen(10)#server can have a backlog connection requests of 10
 
 #b infront means the string is in bytes not UNICODE
-http_response= b"""HTTP/1.1 200 OK\r 
-Content-Type: text/plain\r
-Content-Length: 10\r
+http_response = b"""HTTP/1.1 200 OK\r\n
+Content-Type: text/plain\r\n
+Content-Length: 10\r\n
 \r\n
 Hellllo!\n"""
 
+def respond(conn):
+    request= str(conn.recv(4096))
+    print(request)
+    conn.send(http_response)
+    conn.shutdown(socket.SHUT_RDWR)  # shut down read write
+    conn.close()
+
 try:
     while True:
-        conn, address = s.accept()  # waiting for a connection
-        print(str(conn.recv(4096)))
-        conn.send(http_response)
-        conn.shutdown(socket.SHUT_RDWR)  # shut down read write
-        conn.close()
+        conn, address = server_socket.accept()  # waiting for a connection
+        _thread.start_new_thread(respond, (conn,))
 
 except KeyboardInterrupt:
-    s.close()
+    server_socket.close()
     sys.exit()
