3618a0297d794bd32c02a1599d9e931ff0e66fce
opkpEh <harshpandeykp20@gmail.com>
2024-11-02 03:37:23 +0530

    threading and error management

--- server/main.py
+++ server/main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
+import errno
 import socket
 from pynput import keyboard
 import os
 import sys
-import _thread
+import threading
 
 def get_file_content(filename):
     try:
@@ -18,7 +19,7 @@ def create_response(content):
         response += b"Content-Type: text/html\r\n"
         response += b"Content-Length: 23\r\n"
         response += b"\r\n"
-        response += b"<h1>404 Not Found</h1>\r\n"
+        response += b"<h1>404 Not Found</h1>"
         return response
 
     else:
@@ -27,15 +28,32 @@ def create_response(content):
         response += f"Content-Length: {len(content)}\r\n".encode()
         response += b"<h1>200 OK</h1>\r\n"
         response += b"\r\n"
-        response += content
-        return response
+        return response + content
 
 def on_press(key):
     if key== keyboard.KeyCode.from_char('q'):
         server_socket.close()
-        sys.exit()
+        sys.exit(0)
 
 server_socket = socket.socket()
+
+def handle_client(client_socket, client_address, response):
+    try:
+        request = client_socket.recv(1024).decode()
+        print(f'Handling connection from {client_address}')
+
+        print(f'Received request from :{client_address}:\n{request}')
+        content= get_file_content(response)
+        http_response = create_response(content)
+        client_socket.send(http_response)
+
+    except Exception as e:
+        print(f'Error handling client {client_address}" {str(e)}')
+
+    finally:
+        client_socket.close()
+        print(f'Client {client_address} closed')
+
 
 def start_server(host='0.0.0.0', port=8080, response="response.html"):
     global server_socket
@@ -51,21 +69,29 @@ def start_server(host='0.0.0.0', port=8080, response="response.html"):
         print(f'Server listening on {host}:{port}')
 
         while True:
-            client_socket, client_address = server_socket.accept()
-            print(f'Accepted connection from {client_address}')
+            try:
+                client_socket, client_address = server_socket.accept()
+                print(f'Accepted connection from {client_address}')
 
-            request = client_socket.recv(1024).decode()
-            print(f'Received {request}')
 
-            content = get_file_content(response)
+                #creating a new thread for client:
+                client_thread= threading.Thread(
+                    target=handle_client,
+                    args=(client_socket,client_address, response)
+                )
 
-            response= create_response(content)
-            client_socket.send(response)
+                client_thread.daemon= True #Thread will close when the main program exits
+                client_thread.start()
+                print(f'Started thread for client {client_socket}')
 
-            client_socket.close()
+            except socket.error as e:
+                if e.errno == 9: # Bad file descriptor when socket is closed
+                    break
+                else:
+                    print(f'Error accepting connection {e}')
 
     except KeyboardInterrupt:
-        print("Shutting down the server")
+        print("Shutting down the server...")
     finally:
         server_socket.close()
 
